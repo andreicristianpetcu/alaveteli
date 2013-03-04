85ca8356c023f0ef3915dc22b3283282d844e01d
Nu Va Suparati <dev@nuvasuparati.info>
2013-03-04 22:28:16 +0200

    added sending mail to testing, added romanian translation 95%,  added nuvasuparati email

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,7 @@ config.action_controller.perform_caching             = false
 # Tell ActionMailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
-config.action_mailer.delivery_method = :test
+config.action_mailer.delivery_method = :sendmail
 
 # Disable request forgery protection in test environment 
 config.action_controller.allow_forgery_protection    = false
