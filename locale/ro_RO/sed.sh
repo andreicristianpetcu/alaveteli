dd2e6c7419a7b5bce60ddceead1e18ba1d48a335
Andrei Cristian Petcu <andrei@ceata.org>
2013-07-29 00:33:50 +0300

    small text fix

---
+++ locale/ro_RO/sed.sh
@@ -0,0 +1,10 @@
+sed -i "s/cererea/solicitarea/g" app.po
+sed -i "s/Cererea/Solicitarea/g" app.po
+sed -i "s/cerere/solicitare/g" app.po
+sed -i "s/Cerere/Solicitare/g" app.po
+sed -i "s/cererii/soloicitării/g" app.po
+sed -i "s/Cererii/Soloicitării/g" app.po
+sed -i "s/cereri/solicitări/g" app.po
+sed -i "s/Cereri/Solicitări/g" app.po
+
+
