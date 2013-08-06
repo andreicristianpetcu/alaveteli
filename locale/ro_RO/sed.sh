37e48f194e13f98663a415397c1a32ace5f72262
Andrei Cristian Petcu <andrei@ceata.org>
2013-08-06 08:27:04 +0300

    correspondence

--- locale/ro_RO/sed.sh
+++ locale/ro_RO/sed.sh
@@ -2,8 +2,8 @@ sed -i "s/cererea/solicitarea/g" app.po
 sed -i "s/Cererea/Solicitarea/g" app.po
 sed -i "s/cerere/solicitare/g" app.po
 sed -i "s/Cerere/Solicitare/g" app.po
-sed -i "s/cererii/soloicitării/g" app.po
-sed -i "s/Cererii/Soloicitării/g" app.po
+sed -i "s/cererii/solicitării/g" app.po
+sed -i "s/Cererii/Solicitării/g" app.po
 sed -i "s/cereri/solicitări/g" app.po
 sed -i "s/Cereri/Solicitări/g" app.po
 
